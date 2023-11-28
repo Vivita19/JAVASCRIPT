98bd9a94a266814e6cf727ce3d29a42b8f7e66be
Vivita19 <151692976+Vivita19@users.noreply.github.com>
2023-11-27 23:02:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let num1 = "10";
+let num2 = "5";
+let num3 = "8";
+
+if (num1 <=  num2){
+    console.log ("el numero mayor");
+ 
+}
+else if(num1 >= num3){
+    console.log ("el numero menor")
+}
+let miVariable = 321;
+const MY_VARIABLE = 123;
+
+let miCadena1 = "Hola";
+let miCadena2 = "Mundo";
+
+let primerNumero = 20;
+let segundoNumero = 10
+let tercerNumero = 5
+let mayor = primerNumero > segundoNumero ? primerNumero : segundoNumero;
+let mayor2 = primerNumero > tercerNumero ? primerNumero : tercerNumero;
+let mayor3 = segundoNumero > tercerNumero ? segundoNumero : tercerNumero;
+;
+if (primerNumero > segundoNumero) {
+  mayor = primerNumero;
+} else {
+  mayor = segundoNumero;
+}
+
+let N1 =10;
+let N2 =20;
+let N3 = 5;
+if (N1>N2){
+    console.log("true");
+}
+else (N2<N1)
+      console.log("false");
+if (N3<N1){
+    console.log("true")
+  }
+  else (N2<N3)
+  console.log("false")
+
+miBooleano = "Programación";
+console.log(miBooleano);
+
+console.log("Cual es mayor?")
+let NUM1 = 23
+let NUM2 = 55
+let NUM3 = 66
+
+//determinar el mayor
+if(NUM1>NUM2 && NUM1>NUM3){
+    console.log("El mayor número es:" + NUM1);
+}
+else if(NUM2>NUM1 && NUM2>NUM3){
+    console.log("El mayor número es:" + NUM2);
+}
+else if(NUM3>NUM1 && NUM3>NUM2){
+     console.log("El mayor número es:" +NUM3);
+}
